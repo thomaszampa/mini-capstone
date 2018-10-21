e818ff6dc8d086231e81a14a60bc5ded0340c6bf
thomaszampa <thomas.zampa@gmail.com>
2018-10-21 10:00:47 -0500

    Add validations to name, price, and description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { in: 10..500 }
+
   def is_discounted?
     price > 10 && false
   end
