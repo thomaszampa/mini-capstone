815a731a08ad5f493eee394c5252757dae34b151
thomaszampa <thomas.zampa@gmail.com>
2018-11-01 19:23:27 -0500

    html product creation form built out with header -- index, show, create, and new routes available and working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,28 @@ class ProductsController < ApplicationController
     render "index.html.erb"
   end
 
+  def create
+    @product = Product.new(
+      name: params["name"],
+      price: params["price"].to_i,
+      description: params["description"],
+      supplier_id: params["supplier_id"].to_i
+      )
+    if @product.save
+      Image.create(
+        url: params[:image_url],
+        product_id: @product.id
+        )
+      redirect_to "/products/#{@product_id}"
+    else
+      render json: {errors: @product.errors.full_messages}, status: 422
+    end  
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
     render "show.html.erb"
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,7 @@ Rails.application.routes.draw do
   end
 
   get "/products" => "products#index"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
   get "/products/:id" => "products#show"
 end
